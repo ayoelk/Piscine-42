fe9eb569793f162c9c3d1a26b0f4af6917d4e76c
Ayoub <ton-email-github@example.com>
2026-07-20 00:01:57 +0200

    *FIX ex02 C04

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c,1);
+}
+
+
+void ft_putnbr(int nb)
+{
+    if (nb == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+        return ;
+    }
+    else if(nb < 0)
+    {
+        write(1, "-", 1);
+        ft_putnbr(-nb);
+    }
+    else if(nb >= 0 && nb <= 9)
+    {
+        ft_putchar(nb + '0');
+    }
+    else
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+}
+
+int main()
+{
+    ft_putnbr(1000);
+}
+
