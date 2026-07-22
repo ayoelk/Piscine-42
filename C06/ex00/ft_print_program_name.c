5ff5ea8f518b7d4dfc81b1f6863a719df70d65e8
Ayoub <ton-email-github@example.com>
2026-07-22 16:56:23 +0200

    ex02

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+        write(1,&str[i],1);
+		i++;
+	}
+	
+}
+
+int main( int argc , char **argv)
+{
+	if( argc >= 1);
+	{
+		ft_putstr(argv[0]);
+		write(1,"/n" ,1);
+	}
+}
---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,29 @@
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+        write(1,&str[i],1);
+		i++;
+	}
+	
+}
+
+int main( int argc , char **argv)
+{
+	int i;
+	
+	i = 1;
+	while( i != argc)
+	{
+		ft_putstr(argv[i]);
+		write(1,"/n" ,1);
+		i++;
+	}
+}
